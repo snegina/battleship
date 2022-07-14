e491ea16a6832e6e4214335d9c10744e9ad97d6c
Anna <snegina@yandex.ru>
2022-07-14 18:06:23 +0300

    second commit

--- js.js
+++ js.js
@@ -10,15 +10,21 @@ var isSunk = false;
 var array = [];
 
 while (isSunk == false) {
+    
     guess = prompt("Привет! Это мини игра морской бой. В 7 ячейках по горизонтали ••••••• (от нуля до шести) расположен 3х палубный корабль врага. Твоя задача потопить его за меньшее кол-во попыток. Стреляй! (введи число 0-6):");
-
+    if (array.includes(guess)) {
+        alert("Ты уже стрелял");
+        guesses = guess + 1;
+        continue;
+    }
     if (guess < 0 || guess > 6 || (typeof guess === "string" & isNaN(guess))) {
         alert("Всё же введи число от 0 до 6")
     } else {
-        guesses = guess + 1;
-
+        guesses = guesses + 1;
+        alert(guesses);
         if (guess == location1 || guess == location2 || guess == location3) {
             alert("Попал");
+            array.push(guess);
             hits = hits + 1;
             if (hits == 3) {
                 isSunk = true;
